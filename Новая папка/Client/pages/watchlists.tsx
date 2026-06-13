5312078234d778020f754595c31acd2b2a4f0fe6
angrybeaver231 <148169352+angrybeaver231@users.noreply.github.com>
2026-06-13 11:16:02 +0300

    Delete Новая папка/Client directory

--- Новая папка/Client/pages/watchlists.tsx
+++
@@ -1,9 +0,0 @@
-import { WatchlistsPanel } from "@/components/watchlists-panel";
-
-export default function WatchlistsPage() {
-  return (
-    <div className="space-y-6" data-testid="page-watchlists">
-      <WatchlistsPanel embedded={false} />
-    </div>
-  );
-}
